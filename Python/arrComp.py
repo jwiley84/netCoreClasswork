a0b52b3ad88c9e66c60074280a34558783b4c00f
jwiley84 <jess.m.wailes@gmail.com>
2018-03-05 07:19:33 -0800

    updated arrComp.py

--- Python/arrComp.py
+++ Python/arrComp.py
@@ -1,7 +1,26 @@
-a = [1,2,3,4,5]
-b = [1,2,3,4,5]
+a = 0;
+
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
+list_one2 = [1,2,5,6,5]
+list_two2 = [1,2,5,6,5,3]
+list_one3 = [1,2,5,6,5,16]
+list_two3 = [1,2,5,6,5]
+list_one4 = ['celery','carrots','bread','milk']
+list_two4 = ['celery','carrots','bread','cream']
 
 
-for items in a,b:
-    if (a[item] == b[item]):
-        print("it worked!")
+
+if (len(list_one4) == len(list_two4)):
+    for item in range(0, len(list_two4)):
+        if (list_one4[item] == list_two4[item]):
+            a += 1; 
+else:
+    print("they don't match!")
+
+#print(a)
+#print(len(list_one2))
+if (a == len(list_one4)):
+    print("They match!")
+else:
+    print("they don't match here!")
